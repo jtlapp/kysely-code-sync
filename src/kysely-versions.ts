7942b7204853a003c74688bfeb9a031a2ab11a3c
Joseph T. Lapp <arachnojoe@gmail.com>
2023-05-17 16:10:08 -0400

    feat: show synced version URL

--- src/kysely-versions.ts
+++ src/kysely-versions.ts
@@ -34,7 +34,7 @@ export async function getBaseDownloadUrl(
   // each of the function is individually tested.
   const maxVersions = getMaxVersions(kyselyVersion);
   const nearestVersion = await getClosestKyselyVersion(maxVersions);
-  console.log(`Syncing with Kysely version ${nearestVersion} at`);
+  console.log(`Syncing with Kysely release ${nearestVersion} at`);
   console.log(
     `  https://github.com/kysely-org/kysely/tree/${nearestVersion}\n`
   );
