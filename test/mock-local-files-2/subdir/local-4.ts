3e2952282fe2d799a9886d5509ea77103361acb7
Joseph T. Lapp <arachnojoe@gmail.com>
2023-05-18 08:25:48 -0400

    test: sync code blocks with // comments

--- test/mock-local-files-2/subdir/local-4.ts
+++ test/mock-local-files-2/subdir/local-4.ts
@@ -6,15 +6,15 @@ export const B4 = 2;
 export const C4_different = 3;
 export const D4 = 4;
 /* END SYNCED CODE */
-/* BEGIN SYNCED CODE */
+// BEGIN SYNCED CODE
 export const E4_different = 5;
 export const F4 = 6;
-/* END SYNCED CODE */
+// END SYNCED CODE
 /* BEGIN SYNCED CODE */
 export const G4 = 7;
 export const H4 = 8;
 /* END SYNCED CODE */
-/* BEGIN SYNCED CODE */
+// BEGIN SYNCED CODE
 export const I4 = 9;
 export const J4_different = 10;
-/* END SYNCED CODE */
+// END SYNCED CODE
